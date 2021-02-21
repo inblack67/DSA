51214c5ca730cd140bed3352019355fe78464d41
Aman Bhardwaj <inblack>
2021-02-21 22:52:00 +0530

    linked list

--- DS/LinkedLists/index.ts
+++ DS/LinkedLists/index.ts
@@ -20,14 +20,87 @@ class MyLinkedList {
             // first node
             this.head = newNode;
         } else {
-            console.log(this.head);
-            // this.head.next = newNode;
             let currentNode = this.head;
             while (currentNode.next) {
                 currentNode = currentNode.next;
             }
             currentNode.next = newNode;
         }
+        this.size++;
+    }
+    insertAtPosition (el: number, position: number) {
+        if (position < 0 || position >= this.size) {
+            throw new Error('Invalid Position');
+        }
+
+        const newNode = new MyNode(el);
+        let currentPosition = 0;
+        let currentNode = this.head;
+        let prevNode: MyNode = null;
+        while (currentPosition < position) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            currentPosition++;
+        }
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+        this.size++;
+    }
+    insertAtHead (el: number) {
+        const newNode = new MyNode(el);
+        if (!this.head) {
+            // first node
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.size++;
+    }
+    deleteFirst () {
+        if (!this.head) {
+            throw new Error('Not Allowed');
+        }
+        if (!this.head.next) {
+            this.head = null;
+        } else {
+            this.head = this.head.next;
+        }
+        this.size--;
+    }
+    deleteLast () {
+        if (!this.head) {
+            throw new Error('Not Allowed');
+        }
+        if (!this.head.next) {
+            this.head = null;
+        } else {
+            let currentNode = this.head;
+            let prevNode: MyNode = null;
+            while (currentNode.next) {
+                prevNode = currentNode;
+                currentNode = currentNode.next;
+            }
+            prevNode.next = null;
+        }
+        this.size--;
+    }
+    deleteFormPosition (position: number) {
+        if (position < 0 || position >= this.size) {
+            throw new Error('Invalid Position');
+        }
+
+        let currentPosition = 0;
+        let currentNode = this.head;
+        let prevNode: MyNode = null;
+        while (currentPosition < position) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            currentPosition++;
+        }
+        prevNode.next = currentNode.next;
+        // delete currentNode.next;
+        this.size--;
     }
     display () {
         if (!this.head) {
@@ -41,6 +114,9 @@ class MyLinkedList {
             }
         }
     }
+    getSize () {
+        return this.size;
+    }
 }
 
 const ll1 = new MyLinkedList();
@@ -49,4 +125,15 @@ ll1.append(2);
 ll1.append(3);
 ll1.append(4);
 ll1.append(5);
+ll1.insertAtHead(6);
+ll1.insertAtHead(7);
+ll1.insertAtHead(8);
+ll1.append(9);
+ll1.append(10);
+ll1.append(11);
+ll1.insertAtPosition(99, 10);
+ll1.deleteFirst();
+ll1.deleteLast();
+ll1.deleteFormPosition(9);
 ll1.display();
+console.log(ll1.getSize());
