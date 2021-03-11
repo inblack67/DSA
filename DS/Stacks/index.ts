04de204453c8b6a789f5e469a0b558acf4e8b698
Aman Bhardwaj <inblack>
2021-03-11 11:11:04 +0530

    native stack

--- DS/Stacks/index.ts
+++ DS/Stacks/index.ts
@@ -1,28 +1,39 @@
 class MyStack {
     size: number;
+    length: number;
     stack: number[];
-    constructor () {
+    constructor (size: number) {
         this.stack = [];
-        this.size = 0;
+        this.size = size;
+        this.length = 0;
     }
-
-    push (el: number) {
+    push (el: number): number {
+        if (this.length === this.size) {
+            throw new Error('Stack Overflow');
+        }
         this.stack.push(el);
-        this.size++;
+        return ++this.length;
     }
-    pop () {
-        this.stack.pop();
-        this.size--;
+    pop (): number {
+        if (this.length === 0) {
+            throw new Error('Stack Underflow');
+        }
+        const popped = this.stack.pop();
+        this.length--;
+        return popped;
     }
     display () {
         console.log(this.stack);
     }
     getSize () {
-        return this.size;
+        return this.length;
+    }
+    isFull () {
+        return this.length === this.size;
     }
 }
 
-const s1 = new MyStack();
+const s1 = new MyStack(5);
 s1.push(1);
 s1.push(2);
 s1.push(3);
@@ -30,4 +41,5 @@ s1.push(4);
 s1.push(5);
 s1.pop();
 s1.display();
+console.log(s1.isFull());
 console.log(s1.getSize());
