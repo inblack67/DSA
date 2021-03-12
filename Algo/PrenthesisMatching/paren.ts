e3e492ab74272d19369f9ff287fd6da6022c4604
Aman Bhardwaj <inblack>
2021-03-12 21:40:01 +0530

    parenthesis matching

---
+++ Algo/PrenthesisMatching/paren.ts
@@ -0,0 +1,26 @@
+export const isMatched = (str: string) => {
+    const paren = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    };
+    const stack: string[] = [];
+    const strArr = str.split('');
+    for (let i = 0; i < strArr.length; i++) {
+        const el = strArr[ i ];
+        if (paren[ el ]) {
+            stack.push(el);
+        } else {
+            const top = stack.length - 1;
+            console.log(el);
+            if (el === paren[ stack[ top ] ]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
+
+console.log(isMatched('(([]))[]'));
