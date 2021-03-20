bc2072793390bfdd69b4e55cb173cef1699a746c
Aman Bhardwaj <inblack>
2021-03-20 08:43:59 +0530

    pre post in

--- src/DS/Trees/bst.ts
+++ src/DS/Trees/bst.ts
@@ -35,12 +35,35 @@ class BinarySearchTree {
             if (nextNode!.right) {
                 queue.push(nextNode!.right);
             }
-
+            i++;
+        }
+    };
+    createBST (nums: number[]) {
+        const queue: TreeNode[] = [];
+        this.root = new TreeNode(nums[ 0 ]);
+        queue.push(this.root);
+        let i = 1;
+        while (queue.length > 0 && i < nums.length) {
+            const el = nums[ i ];
+            const nextNode = queue.shift();
+            if (el <= nextNode!.val) {
+                const leftChild = new TreeNode(el);
+                nextNode!.left = leftChild;
+            } else {
+                const rightChild = new TreeNode(el);
+                nextNode!.right = rightChild;
+            }
+            if (nextNode!.left) {
+                queue.push(nextNode!.left);
+            }
+            if (nextNode!.right) {
+                queue.push(nextNode!.right);
+            }
             i++;
         }
     };
 
-    preorder = (root: TreeNode | null) => {
+    preorder (root: TreeNode | null) {
         let curr = root;
         if (curr) {
             console.log(curr.val);
@@ -48,9 +71,26 @@ class BinarySearchTree {
             this.preorder(curr.right);
         }
     };
+    inorder (root: TreeNode | null) {
+        let curr = root;
+        if (curr) {
+            this.inorder(curr.left);
+            console.log(curr.val);
+            this.inorder(curr.right);
+        }
+    };
+    postorder (root: TreeNode | null) {
+        let curr = root;
+        if (curr) {
+            this.postorder(curr.left);
+            this.postorder(curr.right);
+            console.log(curr.val);
+        }
+    };
 }
 
 const bst = new BinarySearchTree();
 // bst.sortedArrayToBST([ 2, 10, -2, 5, 20, 1, 9, 0 ]);
-bst.sortedArrayToBST([ -10, -3, 0, 5, 9 ]);
+// bst.sortedArrayToBST([ -10, -3, 0, 5, 9 ]);
+bst.createBST([ 10, 2, 3, 20, 0, 1, 3 ]);
 bst.preorder(bst.root);
