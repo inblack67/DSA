6e5a68250d42695f4af4ad3eb0bf2f7e5b1c8bc7
Aman Bhardwaj <inblack>
2021-04-01 18:13:46 +0530

    insert in an heap

--- src/DS/Heaps/heaps.ts
+++ src/DS/Heaps/heaps.ts
@@ -1,47 +1,50 @@
 class MaxHeap {
     heap: number[];
     constructor () {
-        this.heap = [];
+        this.heap = [ 20, 10 ];
     }
     getParentIndex (index: number) {
-        const parentIndex = (index - 1) / 2;
-        return Math.floor(parentIndex);
+        const parentIndex = Math.floor((index - 1) / 2);
+        return parentIndex;
     }
-    getLeftChild (index: number) {
-        const leftChildIndex = (2 * index) + 1;
-        return Math.floor(leftChildIndex);
+    getLeftChildInex (index: number) {
+        const leftChildIndex = Math.floor((2 * index) + 1);
+        return leftChildIndex;
     }
-    getRightChild (index: number) {
-        const rightChildIndex = (2 * index) + 2;
-        return Math.floor(rightChildIndex);
+    getRightChildIndex (index: number) {
+        const rightChildIndex = Math.floor((2 * index) + 2);
+        return rightChildIndex;
     }
     swap (index1: number, index2: number) {
         const temp = this.heap[ index1 ];
         this.heap[ index1 ] = this.heap[ index2 ];
         this.heap[ index2 ] = temp;
     }
-    heapify (index: number) {
-        const parentIndex = this.getParentIndex(index);
-        while (this.heap[ index ] > this.heap[ parentIndex ]) {
-            this.swap(index, parentIndex);
-        }
-    }
     insert (el: number) {
         this.heap.push(el);
-        this.heapify(this.heap.length - 1);
+
+        // [ 20, 10, 30 ]
+        let lastIndex = this.heap.length - 1; // 2
+        const lastInsertedEl = this.heap[ lastIndex ]; // 30
+        const parentOfLastInserted = this.heap[ this.getParentIndex(lastIndex) ]; // 20
+        while (lastIndex > 0 && lastInsertedEl > parentOfLastInserted) {
+            console.log('object');
+            // 30 > 20
+            this.heap[ lastIndex ] = this.heap[ this.getParentIndex(lastIndex) ]; // [ 20, 10, 20 ] 
+            lastIndex = this.getParentIndex(lastIndex); // 0
+        }
+        this.heap[ lastIndex ] = lastInsertedEl; // [ 30, 10, 20 ];
     }
     create (arr: number[]) {
-        arr.forEach((el) => {
-            this.insert(el);
-        });
+        console.log(arr);
     }
     display () {
         console.log(this.heap);
     }
 }
-
-const h1 = new MaxHeap();
-const heapData = [ 10, 20, 30 ];
-h1.create(heapData);
-h1.display();
-console.log(heapData);
+const mh1 = new MaxHeap();
+mh1.insert(30);
+mh1.insert(40);
+mh1.insert(5);
+// mh1.create([ 10, 20, 30 ]);
+mh1.display();
