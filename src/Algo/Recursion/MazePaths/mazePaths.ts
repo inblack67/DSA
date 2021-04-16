3c5b65571691496cf8fef1d5c1ab130c9e177078
ryuzaki <ryuzaki>
2021-04-16 16:49:57 +0530

    get maze paths

--- src/Algo/Recursion/MazePaths/mazePaths.ts
+++ src/Algo/Recursion/MazePaths/mazePaths.ts
@@ -3,6 +3,29 @@ const getMazePaths = (
   sourceCol: number,
   destRow: number,
   destCol: number,
-) => {};
+): string[] => {
+  if (destRow === sourceRow && destCol === sourceCol) {
+    return [''];
+  }
+  let horizontalPaths: string[] = [];
+  let verticalPaths: string[] = [];
+  if (destCol < sourceCol) {
+    horizontalPaths = getMazePaths(sourceRow, sourceCol, destRow, destCol + 1);
+  }
+  if (destRow < sourceRow) {
+    verticalPaths = getMazePaths(sourceRow, sourceCol, destRow + 1, destCol);
+  }
+  const res: string[] = [];
+  for (let i = 0; i < horizontalPaths.length; i++) {
+    const el = horizontalPaths[i];
+    res.push(`h${el}`);
+  }
+  for (let i = 0; i < verticalPaths.length; i++) {
+    const el = verticalPaths[i];
+    res.push(`v${el}`);
+  }
+  return res;
+};
 
-console.log(getMazePaths(3, 3, -1, -1));
+const mazePaths = getMazePaths(3, 3, 1, 1); // standing at 0,0 init
+console.log(mazePaths);
