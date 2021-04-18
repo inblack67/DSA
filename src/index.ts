00cdadc65e4cb102d0affe2eeb5a5a30d5eb7e7d
ryuzaki <ryuzaki>
2021-04-18 19:38:14 +0530

    experiments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import { knightsTour } from './Algo/Recursion/KnightsTour/knightsTour';
+
+const getMyChess = (order: number): number[][] => {
+  const chess = new Array(order).fill(new Array(order).fill(0));
+  console.log('my chess = ', chess);
+  return chess;
+};
+
 const main = async () => {
-  console.log("hello");
+  const chess = getMyChess(5);
+  knightsTour(chess, chess.length, 2, 0, 1);
 };
 
 main().catch((err) => console.error(err));
