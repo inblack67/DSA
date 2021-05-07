3cf3899b1811f63f93715474cb4f6223dbb95341
ryuzaki <ryuzaki>
2021-05-07 12:01:17 +0530

    perfect count sort

--- src/Algo/Sorting/CountSort/index.js
+++ src/Algo/Sorting/CountSort/index.js
@@ -11,7 +11,7 @@ const displayArray_2 = (arr) => {
   }
 };
 
-// sorts negative nums too
+// sorts negative nums & stable
 const performCountSort = (arr) => {
   // calc max min
   let min = Number.MAX_SAFE_INTEGER;
@@ -33,14 +33,22 @@ const performCountSort = (arr) => {
     frequency[index]++;
   }
 
-  let j = 0;
+  // converting frequency array to prefix sum array
+  for (let i = 1; i < frequency.length; i++) {
+    frequency[i] += frequency[i - 1];
+  }
 
-  for (let i = 0; i < frequency.length; i++) {
-    let el = frequency[i];
-    while (el > 0) {
-      arr[j++] = i + min;
-      el--;
-    }
+  const ans = new Array(arr.length);
+
+  for (let i = ans.length - 1; i >= 0; i--) {
+    const value = arr[i];
+    const index = frequency[value - min] - 1;
+    ans[index] = value;
+    frequency[value - min]--;
+  }
+
+  for (let i = 0; i < ans.length; i++) {
+    arr[i] = ans[i];
   }
 
   displayArray_2(arr);
--- src/Algo/Sorting/CountSort/index.ts
+++ src/Algo/Sorting/CountSort/index.ts
@@ -12,6 +12,7 @@ const displayArray_2 = (arr: number[]): void => {
   }
 };
 
+// only for positive els
 const performCountSort_2 = (arr: number[]): void => {
   const maxEl = Math.max(...arr);
   const hash: number[] = new Array(maxEl + 1).fill(0);
@@ -28,7 +29,42 @@ const performCountSort_2 = (arr: number[]): void => {
   }
 };
 
-// sorts negative elements too and its stable
+// sorts negative elements too but unstable
+const performCountSort_3 = (arr: number[]): void => {
+  // calc max min
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  // filling up frequency array
+  const frequency: number[] = new Array(max - min + 1).fill(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    const index = arr[i] - min;
+    frequency[index]++;
+  }
+
+  let j = 0;
+
+  for (let i = 0; i < frequency.length; i++) {
+    let el = frequency[i];
+    while (el > 0) {
+      arr[j++] = i + min;
+      el--;
+    }
+  }
+
+  displayArray_2(arr);
+};
+
+// perfect count sort => sorts -ve nums and is stable
 const performCountSort = (arr: number[]): void => {
   // calc max min
   let min = Number.MAX_SAFE_INTEGER;
@@ -50,22 +86,28 @@ const performCountSort = (arr: number[]): void => {
     frequency[index]++;
   }
 
-  console.log(frequency);
+  // converting frequency array to prefix sum array
+  for (let i = 1; i < frequency.length; i++) {
+    frequency[i] += frequency[i - 1];
+  }
 
-  let j = 0;
+  const ans = new Array(arr.length);
 
-  for (let i = 0; i < frequency.length; i++) {
-    let el = frequency[i];
-    while (el > 0) {
-      arr[j++] = i + min;
-      el--;
-    }
+  for (let i = ans.length - 1; i >= 0; i--) {
+    const value = arr[i];
+    const index = frequency[value - min] - 1;
+    ans[index] = value;
+    frequency[value - min]--;
+  }
+
+  for (let i = 0; i < ans.length; i++) {
+    arr[i] = ans[i];
   }
 
   displayArray_2(arr);
 };
 
-// const myArray_2 = [7, -2, 4, 1, 3];
+const myArray_2 = [7, -2, 4, 1, 3];
 // const myArray_2 = [8, 3, 2, 7, 4, 9, 2, 6, 5];
 performCountSort(myArray_2);
 console.log(myArray_2);
