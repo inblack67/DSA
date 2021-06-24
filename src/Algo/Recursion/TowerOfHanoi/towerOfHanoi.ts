4c26ba4082507838c1f13aa45598a79e3d444a4c
ryuzaki <ryuzaki>
2021-06-24 12:33:06 +0530

    tower of hanoi insight

--- src/Algo/Recursion/TowerOfHanoi/towerOfHanoi.ts
+++ src/Algo/Recursion/TowerOfHanoi/towerOfHanoi.ts
@@ -3,9 +3,9 @@ const TOH = (disks: number, tower1: number, tower2: number, tower3: number) => {
     return;
   }
   TOH(disks - 1, tower1, tower3, tower2);
-  console.log(`${tower1} => ${tower3}`);
+  console.log(`${tower1} => ${tower3}`); // this will always be one iteration as the largest disk will be moved => just one that's why no recursion call here
   TOH(disks - 1, tower2, tower1, tower3);
 };
 
 // TOH(2, 1, 2, 3);
-// TOH(3, 10, 11, 12);
+TOH(3, 10, 11, 12);
