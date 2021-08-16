0768dd8f53304208538867341241ed0deff2f3ea
ryuzaki <ryuzaki>
2021-08-16 17:17:36 +0530

    right-most set bit mask

---
+++ src/Algo/BitManipulation/RSBMask/index.ts
@@ -0,0 +1,16 @@
+export const rsbMask = (num: number): string => {
+  // first 1 from the right => rest zero
+  //   num and with 2's complement of num
+  const res = num & -num;
+
+  // You can use Number.toString(2) function, but it has some problems when representing negative numbers. For example, (-1).toString(2) output is "-1".
+
+  // To fix this issue, you can use the unsigned right shift bitwise operator (>>>) to coerce your number to an unsigned integer.
+
+  // If you run (-1 >>> 0).toString(2) you will shift your number 0 bits to the right, which doesn't change the number itself but it will be represented as an unsigned integer
+
+  const binaryRes = (res >>> 0).toString(2);
+  return binaryRes;
+};
+
+console.log(rsbMask(58));
---
+++ src/Algo/BitManipulation/RSBMask/readme.md
@@ -0,0 +1,14 @@
+1. You are given a number n.
+2. You have to print the right-most set bit mask.
+Input Format
+A number n
+Output Format
+A number
+Question Video
+
+  COMMENTConstraints
+1 <= n <= 10^9
+Sample Input
+58
+Sample Output
+10
