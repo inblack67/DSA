40d836b47b4a842de7fcecf5bf7527a0f29e0b1a
ryuzaki <ryuzaki>
2021-09-06 12:02:35 +0530

    nqueens revisited

--- src/Algo/Recursion/NQueensProblem/nQueensProblem.ts
+++ src/Algo/Recursion/NQueensProblem/nQueensProblem.ts
@@ -61,3 +61,59 @@ const nQueensProblem = (
 
 const chess22 = getChess22(4);
 nQueensProblem(chess22, 4, 0, '');
+
+const getMeChess = (order: number): number[][] => {
+  const chess = new Array(order).fill(null).map(() => new Array(order).fill(0));
+  return chess;
+};
+
+const isMyQueenSafe = (
+  chess: number[][],
+  row: number,
+  col: number,
+): boolean => {
+  // above
+  for (let i = row - 1, j = col; i >= 0; i--) {
+    const el = chess[i][j];
+    if (el === 1) {
+      return false;
+    }
+  }
+
+  // left above diagonal
+  for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    const el = chess[i][j];
+    if (el === 1) {
+      return false;
+    }
+  }
+
+  // right above diagonal
+  for (let i = row - 1, j = col + 1; i >= 0 && j < chess[0].length; i--, j++) {
+    const el = chess[i][j];
+    if (el === 1) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const solveNQueens = (chess: number[][], row = 0, ans: string = ''): void => {
+  if (row === chess.length) {
+    console.log(ans);
+    return;
+  }
+
+  for (let i = 0; i < chess[0].length; i++) {
+    const isIt = isMyQueenSafe(chess, row, i);
+    if (isIt) {
+      chess[row][i] = 1;
+      solveNQueens(chess, row + 1, ans + `${row}-${i},`); // always putting one queen in a row
+      chess[row][i] = 0;
+    }
+  }
+};
+
+const myChess = getMeChess(4);
+solveNQueens(myChess);
