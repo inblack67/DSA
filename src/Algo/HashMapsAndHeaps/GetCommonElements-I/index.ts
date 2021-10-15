008103c5c5ac4ebb55f813466f679f54da666776
aman <aman>
2021-10-15 20:40:01 +0530

    printCommonElements => better code

--- src/Algo/HashMapsAndHeaps/GetCommonElements-I/index.ts
+++ src/Algo/HashMapsAndHeaps/GetCommonElements-I/index.ts
@@ -1,26 +1,14 @@
 const printCommonElements = (arr1: number[], arr2: number[]): void => {
-  const hash1 = new Map<number, number>();
-  const hash2 = new Map<number, number>();
-  arr1.forEach((el) => {
-    const count = hash1.get(el);
-    if (count === undefined) {
-      hash1.set(el, 1);
-    } else {
-      hash1.set(el, count + 1);
-    }
-  });
+  const hashMap1 = new Map<number, boolean>();
+  const hashMap2 = new Map<number, boolean>();
+
+  arr1.forEach((el) => hashMap1.set(el, true));
+  arr2.forEach((el) => hashMap2.set(el, true));
+
   arr2.forEach((el) => {
-    const count = hash2.get(el);
-    if (count === undefined) {
-      hash2.set(el, 1);
-    } else {
-      hash2.set(el, count + 1);
-    }
-  });
-  hash2.forEach((_, key) => {
-    const hash1Query = hash1.get(key);
-    if (hash1Query) {
-      console.log(key);
+    if (hashMap1.has(el)) {
+      console.log(el);
+      hashMap1.delete(el);
     }
   });
 };
