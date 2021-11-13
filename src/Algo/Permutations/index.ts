2cea5a3d80b59586a4abc1f9f68920bd4015fe81
aman <aman>
2021-11-13 08:58:14 +0530

    get array permutations

--- src/Algo/Permutations/index.ts
+++ src/Algo/Permutations/index.ts
@@ -13,3 +13,24 @@ const printArrayPermutations = (nums: number[], res: string = ''): void => {
 };
 
 printArrayPermutations([1, 2, 3]);
+
+const getArrayPermutations = (arr: number[]): number[][] => {
+  if (arr.length === 0) {
+    return [];
+  } else if (arr.length === 1) {
+    return [[arr[0]]];
+  }
+  const res: number[][] = [];
+  for (let index = 0; index < arr.length; index++) {
+    const current = arr[index];
+    const left = arr.slice(0, index);
+    const right = arr.slice(index + 1);
+    const newArr = [...left, ...right];
+    const soFar = getArrayPermutations(newArr);
+    soFar.forEach((el) => el.unshift(current));
+    res.push(...soFar);
+  }
+  return res;
+};
+
+console.log(getArrayPermutations([1, 2, 3]));
